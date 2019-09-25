e3d83d4ffc469f0e543ec05365ddb97d322ce8c2
unknown <lusiapettie@gmail.com>
2019-09-25 20:10:54 +0300

    initial commit

---
+++ aboutus.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> LE'KIONDO</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    
+     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="dist/jquery.scrollUp.js"></script>
+ <script type="text/javascript" src="dist/demo.js"></script>
+    
+    
+    </head>
+    
+<body>
+    <header class="tm-header">
+        <img class="tm-logo" src="images/logo.jpg" alt=>
+        <nav class="tm-nav">
+       <div>
+            <ul>
+                <li class="tm-list"><a href="index.html">Home</a></li>
+                <li class="tm-list"><a href="explore.html">Explore</a></li>
+                <li class="tm-list"><a href="about.html">About us</a></li>
+                <li class="tm-list"><a href="price.html">Pricing</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>    
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
